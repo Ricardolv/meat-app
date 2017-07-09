7731438a0b1bd049e808fead23f37a02b8d9c854
Ricardolv <richardluizv@gmail.com>
2017-07-08 23:28:28 -0300

    implementando itens de menu

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
 import {Restaurant} from './restaurant/restaurant.model'
+import {MenuItem} from '../restaurant-detail/menu-item/menu-item.model';
 
 import {MEAT_API} from '../app.api'
 import {ErrorHandler} from '../app.error-handler'
@@ -27,8 +28,14 @@ export class RestaurantsService {
         .catch(ErrorHandler.handlerError)
   }
 
-  reviewsOfRestaurant(id: string) : Observable<any> {
+  reviewsOfRestaurant(id: string): Observable<any> {
     return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handlerError)
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
         .map(response => response.json())
         .catch(ErrorHandler.handlerError)
   }
