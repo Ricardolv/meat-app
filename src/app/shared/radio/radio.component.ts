b0cafc7f24a59f99d616a2cb6886c6c66e17aad9
Ricardolv <richardluizv@gmail.com>
2017-07-10 18:32:13 -0300

    criando componente usando controlValueAccessor

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 import { RadioOption } from './radio-option.model';
 
+/**
+    E meio estranho esta configuracao passando no providers:
+      " forwardRef, NG_VALUE_ACCESSOR, ControlValueAccessor "
+    mas e forma de configurar o componente como um value acessor,
+    que fique disponivel para as diretivas ngModel e outras diretivas
+    do modulo reactive forms.
+*/
+
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      //Declarando um referencia no componente
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: RadioOption[]
+
   value: any
+
+  onChange: any
 
   constructor() { }
 
@@ -18,6 +38,28 @@ export class RadioComponent implements OnInit {
 
   setValue(value: any) {
     this.value = value
+    this.onChange(this.value)
   }
 
+  /**
+  * Metodo responsavel por ser chamado pelas diretivas,
+  * quando elas querem passar um valor para o seu componentes.
+  */
+  writeValue(obj: any) : void {
+    this.value = obj
+  }
+
+  /**
+  * Metodo responsavel por receber uma funcao sempre que o valor interno mudar.
+  */
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  /**
+  * Metodo responsavel por registrar que o usuario entrou no componente.
+  */
+  registerOnTouched(fn: any): void { }
+
+  setDisabledState?(isDisabled: boolean): void {}
 }
